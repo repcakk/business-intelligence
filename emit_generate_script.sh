8587730ad494e01ed1999cdfc1da7076db8b6ee7
Marek Marecki <marekjm@ozro.pw>
2020-01-16 16:49:13 +0100

    Generate first sample data
    Covered tables:
    
    - W_data
    - W_projekt
    - W_pracownik

--- emit_generate_script.sh
+++ emit_generate_script.sh
@@ -6,11 +6,50 @@ GENERATE_SCRIPT=generate_sample_data.py
 
 echo '#!/usr/bin/env python3
 
+import datetime
 import random
+
+
+# Generic generators
+
+# TODO
+
+# Table generators
+def inhibit_emission(values):
+    return values[0] == "()"
 
 ' > $GENERATE_SCRIPT
 
 cat hd-database/*.sql |
+    sort |
     grep -P 'CREATE' |
-    sed 's/.*\[\([FW]_.*\)\]/def \1():\n    pass\n/' |
+    sed 's/.*\[\([FW]_.*\)\]/def \1():\
+    insert_statement = "INSERT INTO \1 values\\n  {}\\n;"\
+\
+    values = []\
+    for i in range(10):\
+        values_line = "()".format(\
+        )\
+        values.append(values_line)\
+\
+    if inhibit_emission(values): return\
+    print(insert_statement.format("\\n, ".join(values)))\
+/' |
     tee -a $GENERATE_SCRIPT > /dev/null
+
+echo '
+
+# Main function
+
+def main():' >> $GENERATE_SCRIPT
+cat hd-database/*.sql |
+    sort |
+    grep -P 'CREATE' |
+    sed 's/.*\[\([FW]_.*\)\]/    \1()/' |
+    tee -a $GENERATE_SCRIPT > /dev/null
+
+echo -n '
+
+if __name__ == "__main__":
+    main()
+' >> $GENERATE_SCRIPT
--- generate_sample_data.py
+++ generate_sample_data.py
@@ -1,29 +1,291 @@
 #!/usr/bin/env python3
 
+import datetime
 import random
 
 
+# Generic generators
+
+IMIONA = (
+    'Adam',
+    'Adrianna',
+    'Bartek',
+    'Basia',
+    'Cezary',
+    'Cecylia',
+    'Damian',
+    'Daria',
+    'Dagmara',
+    'Ernest',
+    'Emilia',
+    'Franek',
+    'Francja',
+    'Grzesiek',
+    'Grażyna',
+    'Kacper',
+    'Marek',
+    'Zbyszek',
+    'Zuzanna',
+)
+
+NAZWISKA = (
+    'Adamowicz',
+    'Bem',
+    'Cerkask+',
+    'Dworsk+',
+    'Testowsk+',
+    'Kleczewsk+',
+    'Mareck+',
+    'Kowalsk+',
+    'Iwanienko',
+)
+
+def imie_nazwisko():
+    imie = random.choice(IMIONA)
+    nazwisko = random.choice(NAZWISKA)
+
+    if nazwisko[-1] == '+':
+        nazwisko = nazwisko[:-1] + ('a' if imie[-1] == 'a' else 'i')
+
+    return '{} {}'.format(imie, nazwisko)
+
+def pesel():
+    y = random.randint(70, 100)
+    m = (lambda x: ('0{}' if x < 10 else '{}').format(x))(random.randint(1, 13))
+    d = (lambda x: ('0{}' if x < 10 else '{}').format(x))(random.randint(1, 32))
+    filler = random.randint(10000, 100000)
+    return '{}{}{}{}'.format(y, m, d, filler)
+
+# Table generators
+def inhibit_emission(values):
+    return values[0] == "()"
+
+
 def F_pracownik_w_projekcie():
-    pass
+    insert_statement = "INSERT INTO F_pracownik_w_projekcie values\n  {}\n;"
+
+    values = []
+    for i in range(10):
+        values_line = "()".format(
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
 
 def F_technologie_pracownika():
-    pass
+    insert_statement = "INSERT INTO F_technologie_pracownika values\n  {}\n;"
+
+    values = []
+    for i in range(10):
+        values_line = "()".format(
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
 
 def W_data():
-    pass
+    insert_statement = "INSERT INTO W_data values\n  {}\n;"
+
+    PORA_ROKU = {
+        0: 'wiosna',
+        1: 'lato',
+        2: 'jesień',
+        3: 'zima',
+    }
+    MIESIACE = {
+        0: { 3: 'marzec', 4: 'kwiecień', 5: 'maj', }, # wiosna
+        1: { 6: 'czerwiec', 7: 'lipiec', 8: 'sierpień', }, # lato
+        2: { 9: 'wrzesień', 10: 'październik', 11: 'listopad', }, # jesień
+        3: { 12: 'grudzień', 1: 'styczeń', 2: 'luty', }, # zima
+    }
+    DNI = {
+         1: 31,
+         2: 28,
+         3: 31,
+         4: 30,
+         5: 31,
+         6: 30,
+         7: 31,
+         8: 31,
+         9: 30,
+        10: 31,
+        11: 30,
+        12: 31,
+    }
+
+    lower_dates = []
+    N = 20
+    for i in range(N):
+        y = random.randint(2014, 2018)
+
+        p = random.randint(0, 3)
+        pp = PORA_ROKU[p]
+
+        m = random.choice(list(MIESIACE[p].keys()))
+        mp = MIESIACE[p][m]
+
+        d = random.randint(1, DNI[m])
+
+        dt = datetime.datetime.strptime(
+            '{}-{}-{}'.format(y, m, d),
+            '%Y-%m-%d',
+        )
+        values_line = "({rok}, {pora_roku}, {miesiac}, {dzien})".format(
+            rok = y,
+            pora_roku = repr(pp),
+            miesiac = repr(mp),
+            dzien = d,
+        )
+        lower_dates.append((i + 1, values_line, dt))
+
+    upper_dates = []
+    for i in range(N // 2):
+        y = random.randint(2019, 2020)
+
+        p = random.randint(0, 3)
+        pp = PORA_ROKU[p]
+
+        m = random.choice(list(MIESIACE[p].keys()))
+        mp = MIESIACE[p][m]
+
+        d = random.randint(1, DNI[m])
+
+        dt = datetime.datetime.strptime(
+            '{}-{}-{}'.format(y, m, d),
+            '%Y-%m-%d',
+        )
+        values_line = "({rok}, {pora_roku}, {miesiac}, {dzien})".format(
+            rok = y,
+            pora_roku = repr(pp),
+            miesiac = repr(mp),
+            dzien = d,
+        )
+        upper_dates.append((i + 1, values_line, dt))
+
+    values = []
+    values.extend(lower_dates)
+    values.extend(upper_dates)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(map(lambda x: x[1], values))))
+
+    return lower_dates, upper_dates
 
 def W_klient():
-    pass
+    insert_statement = "INSERT INTO W_klient values\n  {}\n;"
+
+    values = []
+    for i in range(10):
+        values_line = "()".format(
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
 
 def W_pracownik():
-    pass
+    insert_statement = "INSERT INTO W_pracownik values\n  {}\n;"
 
-def W_projekt():
-    pass
+    values = []
+    for i in range(10):
+        values_line = '({imie_nazwisko}, {pesel}, {doswiadczenie}, {wynagrodzenie}, {szef_id})'.format(
+            imie_nazwisko = repr(imie_nazwisko()),
+            pesel = pesel(),
+            doswiadczenie = repr(random.choice(('junior', 'regular', 'senior',))),
+            wynagrodzenie = repr(random.choice(('niskie', 'srednie', 'wysokie',))),
+            szef_id = 1,  # nieistorne, bo nie pojawia się w pytaniach
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
+
+def W_projekt(lower_dates, upper_dates):
+    insert_statement = "INSERT INTO W_projekt values\n  {}\n;"
+
+    STARTUP_TEMPLATE = (
+        'Uber for {}',
+        'Super {}',
+        'Space {}',
+        '{} X',
+        'Turbo {}',
+        '{} 3000',
+        'New-Generation {}',
+    )
+    WHAT_FOR = (
+        'Food',
+        'Hamsters',
+        'Data Warehsouses',
+        'Pascal',
+        'Kelvin',
+        'Watt',
+        'University',
+        'Academia',
+        'Industry',
+    )
+    def nazwa():
+        return random.choice(STARTUP_TEMPLATE).format(random.choice(WHAT_FOR))
+
+    values = []
+    for i in range(10):
+        data_rozpoczecia = random.choice(lower_dates)
+        data_zakonczenia = random.choice(lower_dates + upper_dates)
+        while data_rozpoczecia[2] >= data_zakonczenia[2]:
+            data_zakonczenia = random.choice(lower_dates + upper_dates)
+
+        values_line = "({nazwa}, {tempo_realizacji}, {liczba_pracownikow}, {czas_trwania}, {data_rozpoczecia}, {data_zakonczenia})".format(
+            nazwa = repr(nazwa()),
+            tempo_realizacji = repr(random.choice(('przed czasem', 'o czasie', 'po czasie',))),
+            liczba_pracownikow = repr(random.choice(('mała', 'średnia', 'duża',))),
+            czas_trwania = (data_zakonczenia[2] - data_rozpoczecia[2]).days,
+            data_rozpoczecia = data_rozpoczecia[0],
+            data_zakonczenia = data_zakonczenia[0],
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
 
 def W_smieci():
-    pass
+    insert_statement = "INSERT INTO W_smieci values\n  {}\n;"
+
+    values = []
+    for i in range(10):
+        values_line = "()".format(
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
 
 def W_technologia():
-    pass
+    insert_statement = "INSERT INTO W_technologia values\n  {}\n;"
 
+    values = []
+    for i in range(10):
+        values_line = "()".format(
+        )
+        values.append(values_line)
+
+    if inhibit_emission(values): return
+    print(insert_statement.format("\n, ".join(values)))
+
+
+
+# Main function
+
+def main():
+    F_pracownik_w_projekcie()
+    F_technologie_pracownika()
+    lower_dates, upper_dates = W_data()
+    W_klient()
+    W_pracownik()
+    W_projekt(lower_dates, upper_dates)
+    W_smieci()
+    W_technologia()
+
+
+if __name__ == "__main__":
+    main()
